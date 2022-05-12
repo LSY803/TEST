4fdf277dc9658aa4a9353500902502bae6f0d4ba
LSY803 <103938408+LSY803@users.noreply.github.com>
2022-05-12 12:29:33 +0800

    text3

--- Assets/script/PlayeCtrl.cs
+++ Assets/script/PlayeCtrl.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayeCtrl : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float MoveForce = 100.0f;
     public float MaxSpeed = 5;
     public Rigidbody2D HeroBody;
@@ -11,13 +13,14 @@ public class PlayeCtrl : MonoBehaviour
     public bool bFaceRight = true;
     [HideInInspector]
     public bool bJump = false;
-    public float JumpForce = 400;
+    public float JumpForce = 100;
     private Transform mGroundCheck;
-    GameObject ufo;
+    Animator anim;
     void Start()
     {
         HeroBody = GetComponent<Rigidbody2D>();
         mGroundCheck = transform.Find("GroundCheck");
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,25 +34,29 @@ public class PlayeCtrl : MonoBehaviour
 
         if (Mathf.Abs(HeroBody.velocity.x) > 5)
         {
-            HeroBody.velocity = new Vector2(Mathf.Sign(HeroBody.velocity.x) * MaxSpeed, HeroBody.velocity.y);
+            HeroBody.velocity = new Vector2(Mathf.Sign(HeroBody.velocity.x) * MaxSpeed,
+                                            HeroBody.velocity.y);
         }
-
+        anim.SetFloat("speed", Mathf.Abs(h));
         if (h > 0 && !bFaceRight)
         {
             flip();
         }
-
         else if (h < 0 && bFaceRight)
         {
             flip();
         }
-        if (Physics2D.Linecast(transform.position, mGroundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+
+        if (Physics2D.Linecast(transform.position, mGroundCheck.position,
+                                1 << LayerMask.NameToLayer("Ground")))
         {
             if (Input.GetButtonDown("Jump"))
             {
                 bJump = true;
             }
         }
+        Debug.DrawLine(transform.position, mGroundCheck.position, Color.red);
+
     }
 
     private void FixedUpdate()
@@ -58,6 +65,7 @@ public class PlayeCtrl : MonoBehaviour
         {
             HeroBody.AddForce(Vector2.up * JumpForce);
             bJump = false;
+            anim.SetTrigger("jump");
         }
     }
 
